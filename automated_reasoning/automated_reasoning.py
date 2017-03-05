5b65652676d603c7fdc611385acee1054c7bd5c8
ukurumba <ukurumba@u.rochester.edu>
2017-03-05 18:31:29 -0500

    added boolean fxs and ttentails fx

--- automated_reasoning/automated_reasoning.py
+++ automated_reasoning/automated_reasoning.py
@@ -1,22 +1,128 @@
 import numpy as np 
 
+def Implies(inputvals):
+	P,Q = inputvals
+	if P == 1 and Q == -1:
+		return -1
+	elif P == 1 and Q == 0:
+		return 0
+	elif P == 1 and Q == 1:
+		return 1
+	elif P == 0:
+		return 0
+	elif P == -1:
+		return 1
+
+def Iff(inputvals):
+	P,Q=inputvals
+	if P == 1 and Q == 1:
+		return 1
+	elif P == -1 and Q == -1:
+		return 1
+	elif P == 0 or Q == 0:
+		return 0
+	else: 
+		return -1
+
+def And(inputvals):
+	P,Q=inputvals
+	if P == 1 and Q == 1:
+		return 1
+	elif P == -1 or Q == -1:
+		return -1
+	elif P == 0 or Q == 0:
+		return 0
+	else:
+		return -1
+
+def Or(inputvals):
+	P,Q=inputvals
+	if P == 1 or Q == 1:
+		return 1
+
+	elif P == -1 and Q == -1:
+		return -1
+
+	else:
+		return 0
+
+def true(P): #a unary sentence that says P is true. Thus if P=False is entered, this sentence returns False.
+	return P[0]
+
+def false(P):
+	return -1 * P[0]
+
+
+
+
+
 class KB(object):
 
 	default_model = []
-	def __init__(self,current_model=default_model):
-		self.model = current_model #initialize KB with a given model
 
-	def implies(self,bool1,bool2):
-		if bool1 == True and bool2 == False:
+	def __init__(self,current_model={},init_sentences=[]):
+		self.model = current_model #initialize KB with a given model
+		self.sentences = init_sentences 
+
+	def add_sentence(self,sentence):
+		sentences.append(sentence)
+
+	def eval_sentences(self):
+		'''This method determines whether or not the specific situation (conceptualized as a knwoledge badse with a model and set of sentences) COULD be true.
+		i.e. if it is possible for the given set of sentences to be true under the current model the function returns True.''' 
+		eval_vals = [1] #so that first evaluation just returns truth-value of first sentence in KB
+		index=0
+		for each_sentence in self.sentences:
+			index+=1
+			eval_vals.append(each_sentence[0]([self.model[i] for i in each_sentence[1]]))
+			eval_vals = [And(eval_vals)] #checking to make sure every sentence in KB evaluates to true 
+			if eval_vals[0] == -1:
+				break
+		if eval_vals[0] == -1:
 			return False
 		else: 
-			return True
+			return True 
 
-	def iff(self,bool1,bool2):
-		if bool1 == bool2:
-			return True
+def make_0(model):
+	for i in model:
+		model[i] = 0
+	return model
+
+def ttentails(kb,alpha):
+	'''This function tests whether or not the sentence alpha is entailed by the sentences in kb.'''
+	symbols = list(kb.model.keys()) # returns a list of all model parameters. 
+	return ttcheckall(kb,alpha,symbols,make_0(kb.model)) #make_0(model) returns a model where every variable is "unknown" i.e. 0 in value
+
+def ttcheckall(kb,alpha,symbols,model):
+	if not symbols: #if symbols is empty
+		if KB(model,kb.sentences).eval_sentences():
+			return KB(model,alpha).eval_sentences()
 		else:
-			return False
+			return True #always implies True if the first object is False
+	else: 
+		P = symbols[0]
+		Rest = symbols[1:]
+		model_P_true = model_P_false = model
+		model_P_true[P] = 1
+		model_P_false[P] = -1
+		return ttcheckall(kb,alpha,Rest,model_P_true) and ttcheckall(kb,alpha,Rest,model_P_false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- tests/tests.py
+++ tests/tests.py
@@ -28,34 +28,59 @@ class TestAutoReason(unittest.TestCase):
         pass
 
     # def test_or(self):
-    # 	self.assertTrue(ar.kb.Or(True,False))
-    # 	self.assertFalse(ar.kb.Or(False,False))   
-    # 	self.assertTrue(ar.kb.Or(True,True))
-    # 	self.assertTrue(ar.kb.Or(False,True)) 
+    # 	self.assertTrue(ar.Or(True,False))
+    # 	self.assertFalse(ar.Or(False,False))   
+    # 	self.assertTrue(ar.Or(True,True))
+    # 	self.assertTrue(ar.Or(False,True)) 
 
     # def test_and(self):
-    # 	self.assertFalse(ar.kb.And(True,False))
-    # 	self.assertFalse(ar.kb.And(False,False))   
-    # 	self.assertTrue(ar.kb.And(True,True))
-    # 	self.assertFalse(ar.kb.And(False,True)) 
+    # 	self.assertFalse(ar.And(True,False))
+    # 	self.assertFalse(ar.And(False,False))   
+    # 	self.assertTrue(ar.And(True,True))
+    # 	self.assertFalse(ar.And(False,True)) 
 
     # def test_not(self):
-    # 	self.assertTrue(ar.kb.Not(False))
-    # 	self.assertFalse(ar.kb.Not(True))
+    # 	self.assertTrue(ar.Not(False))
+    # 	self.assertFalse(ar.Not(True))
 
-    def test_implies(self):
-    	kb = ar.KB()
-    	self.assertTrue(kb.implies(False,False))
-    	self.assertFalse(kb.implies(True,False))   
-    	self.assertTrue(kb.implies(True,True))
-    	self.assertTrue(kb.implies(False,True)) 
+    def test_booleans(self):
+    	possible_cases = [[1,1],[1,0],[1,-1],[0,1],[0,0],[0,-1],[-1,1],[-1,0],[-1,-1]]
+    	iff_val = [1,0,-1,0,0,0,-1,0,1]
+    	implies_val = [1,0,-1,0,0,0,1,1,1]
+    	and_val = [1,0,-1,0,0,-1,-1,-1,-1]
+    	or_val = [1,1,1,1,0,0,1,0,-1]
+    	index=0
+    	for case in possible_cases:
+    		self.assertEqual(iff_val[index],ar.Iff(case))
+    		self.assertEqual(implies_val[index],ar.Implies(case))
+    		self.assertEqual(and_val[index],ar.And(case))
+    		self.assertEqual(or_val[index],ar.Or(case))
+    		index+=1
+    
+    def test_eval_sentences(self):
+    	model = {'P' : 1,'Q' : -1,'R' : 0, 'S' : 1}
+    	sentences = [[ar.Implies,['P','Q']],[ar.And,['R','S']],[ar.Or,['Q','P']]]
+    	sentences2 =  [[ar.Implies,['P','S']],[ar.And,['R','S']],[ar.Or,['Q','P']],[ar.false,['Q']]]
+    	sentences3 = [[ar.Implies,['P','S']],[ar.And,['P','S']],[ar.Or,['Q','P']],[ar.true,['P']]]
 
-    def test_iff(self):
-    	kb = ar.KB()
-    	self.assertFalse(kb.iff(True,False))
-    	self.assertTrue(kb.iff(False,False))   
-    	self.assertTrue(kb.iff(True,True))
-    	self.assertFalse(kb.iff(False,True)) 
+    	kb = ar.KB(model,sentences)
+    	kb2 = ar.KB(model,sentences2)
+    	kb3 = ar.KB(model,sentences3)
+    	self.assertEqual(kb.eval_sentences(),False)
+    	self.assertEqual(kb2.eval_sentences(),True)
+    	self.assertEqual(kb3.eval_sentences(),True)
+
+    def test_tt_entails(self):
+    	model = {'P' : 1,'Q' : -1,'R' : 0, 'S' : 1}
+    	sentences = [[ar.Implies,['P','Q']],[ar.Implies,['Q','R']],[ar.true,['P']]]
+    	kb =ar.KB(model,sentences)
+    	alpha = [[ar.true,['R']]]
+    	alpha2 = [[ar.true,['S']]]
+    	self.assertTrue(ar.ttentails(kb,alpha))
+    	self.assertFalse(ar.ttentails(kb,alpha2))
+
+
+
 
         
 tests = unittest.TestLoader().loadTestsFromTestCase(TestAutoReason)
