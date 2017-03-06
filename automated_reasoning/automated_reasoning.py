b89a729490cdac37bbd4fd2ccaa4017b74713e9c
ukurumba <ukurumba@u.rochester.edu>
2017-03-06 17:08:27 -0500

    finished questions 1 and 2 of part 1

--- automated_reasoning/automated_reasoning.py
+++ automated_reasoning/automated_reasoning.py
@@ -1,56 +1,54 @@
-import numpy as np 
-
 def Implies(inputvals):
-	P,Q = inputvals
-	if P == 1 and Q == -1:
-		return -1
-	elif P == 1 and Q == 0:
-		return 0
-	elif P == 1 and Q == 1:
-		return 1
-	elif P == 0:
-		return 0
-	elif P == -1:
-		return 1
+    P,Q = inputvals
+    if P == 1 and Q == -1:
+        return -1
+    elif P == 1 and Q == 0:
+        return 0
+    elif P == 1 and Q == 1:
+        return 1
+    elif P == 0:
+        return 0
+    elif P == -1:
+        return 1
 
 def Iff(inputvals):
-	P,Q=inputvals
-	if P == 1 and Q == 1:
-		return 1
-	elif P == -1 and Q == -1:
-		return 1
-	elif P == 0 or Q == 0:
-		return 0
-	else: 
-		return -1
+    P,Q=inputvals
+    if P == 1 and Q == 1:
+        return 1
+    elif P == -1 and Q == -1:
+        return 1
+    elif P == 0 or Q == 0:
+        return 0
+    else: 
+        return -1
 
 def And(inputvals):
-	P,Q=inputvals
-	if P == 1 and Q == 1:
-		return 1
-	elif P == -1 or Q == -1:
-		return -1
-	elif P == 0 or Q == 0:
-		return 0
-	else:
-		return -1
+    P,Q=inputvals
+    if P == 1 and Q == 1:
+        return 1
+    elif P == -1 or Q == -1:
+        return -1
+    elif P == 0 or Q == 0:
+        return 0
+    else:
+        return -1
 
 def Or(inputvals):
-	P,Q=inputvals
-	if P == 1 or Q == 1:
-		return 1
+    P,Q=inputvals
+    if P == 1 or Q == 1:
+        return 1
 
-	elif P == -1 and Q == -1:
-		return -1
+    elif P == -1 and Q == -1:
+        return -1
 
-	else:
-		return 0
+    else:
+        return 0
 
 def true(P): #a unary sentence that says P is true. Thus if P=False is entered, this sentence returns False.
-	return P[0]
+    return P[0]
 
 def false(P):
-	return -1 * P[0]
+    return -1 * P[0]
 
 
 
@@ -58,99 +56,81 @@ def false(P):
 
 class KB(object):
 
-	default_model = []
+    default_model = []
 
-	def __init__(self,current_model={},init_sentences=[]):
-		self.model = current_model #initialize KB with a given model
-		self.sentences = init_sentences 
+    def __init__(self,current_model={},init_sentences=[]):
+        self.model = current_model #initialize KB with a given model
+        self.sentences = init_sentences 
 
-	def add_sentence(self,sentence):
-		sentences.append(sentence)
+    def add_sentence(self,sentence):
+        sentences.append(sentence)
 
-	
-	def multi_eval(sent,model):
 
-	    if type(sent[0])!=type(multi_eval): # for the collection of sentences in KB
-	        val = 1
-	        for element in sent:
-	            val = And([val,multi_eval(element,model)])
-	            if val == -1:
-	                break
-	        return val
-	    else:
-	        if sent[0]!= true and sent[0] != false: #for the case where there are 3 elements in the list
-	            if (type(sent[1]) == str or type(sent[1]) == int) and (type(sent[2]) == str or type(sent[2]) == int):
-	                return eval(sent,model)
-	            elif (type(sent[1]) == list) and (type(sent[2]) == int or type(sent[2]) == str):
-	                return eval([sent[0],multi_eval(sent[1],model),sent[2]],model)
-	            else: # if both arguments are non-unary sentences:
-	                return eval([sent[0],multi_eval(sent[1],model),multi_eval(sent[2],model)],model)
-	        else:
-	            if (type(sent[1]) == str or type(sent[1]) == int):
-	                return eval(sent,model)
-	            elif (type(sent[1]) == list):
-	                return eval([sent[0],multi_eval(sent[1],model)],model)
-	     
-	    
+    def multi_eval(self,sent,model):
+        if type(sent[0])!=type(Iff): # for the collection of sentences in KB
+            val = 1
+            for element in sent:
+                val = And([val,self.multi_eval(element,model)])
+                if val == -1:
+                    break
+            return val
+        else:
+            if sent[0]!= true and sent[0] != false: #for the case where there are 3 elements in the list
+                if (type(sent[1]) == str or type(sent[1]) == int) and (type(sent[2]) == str or type(sent[2]) == int):
+                    return self.eval(sent,model)
+                elif (type(sent[1]) == list) and (type(sent[2]) == int or type(sent[2]) == str):
+                    return self.eval([sent[0],self.multi_eval(sent[1],model),sent[2]],model)
+                elif (type(sent[1]) == int or type(sent[1]) == str) and (type(sent[2]) == list):
+                    return self.eval([sent[0],sent[1],self.multi_eval(sent[2],model)],model)
+                else: # if both arguments are non-unary sentences:
+                    return self.eval([sent[0],self.multi_eval(sent[1],model),self.multi_eval(sent[2],model)],model)
+            else:
+                if (type(sent[1]) == str or type(sent[1]) == int):
+                    return self.eval(sent,model)
+                elif (type(sent[1]) == list):
+                    return self.eval([sent[0],self.multi_eval(sent[1],model)],model)
 
-	def eval(sentence):
-	    for i in range(1,len(sentence)):
-	        if type(sentence[i]) == str:
-	            sentence[i] = self.model[sentence[i]]
-	    if len(sentence) == 2:
-	        return sentence[0]([sentence[1]])
-	    elif len(sentence) == 3:
-	        return sentence[0]([sentence[1],sentence[2]])
 
-    
-    
-    
+
+    def eval(self,sentence,model):
+        sentence = sentence.copy()
+        for i in range(1,len(sentence)):
+            if type(sentence[i]) == str:
+                sentence[i] = model[sentence[i]]
+        if len(sentence) == 2:
+            return sentence[0]([sentence[1]])
+        elif len(sentence) == 3:
+            return sentence[0]([sentence[1],sentence[2]])
+
+
+
+
 
 def make_0(model):
-	for i in model:
-		model[i] = 0
-	return model
+    for i in model:
+        model[i] = 0
+    return model
 
 def ttentails(kb,alpha):
-	'''This function tests whether or not the sentence alpha is entailed by the sentences in kb.'''
-	symbols = list(kb.model.keys()) # returns a list of all model parameters. 
-	return ttcheckall(kb,alpha,symbols,make_0(kb.model)) #make_0(model) returns a model where every variable is "unknown" i.e. 0 in value
+    '''This function tests whether or not the sentence alpha is entailed by the sentences in kb.'''
+    symbols = list(kb.model.keys()) # returns a list of all model parameters. 
+    return ttcheckall(kb,alpha,symbols,make_0(kb.model)) #make_0(model) returns a model where every variable is "unknown" i.e. 0 in value
 
 def ttcheckall(kb,alpha,symbols,model):
-	if not symbols: #if symbols is empty
-		if KB(model,kb.sentences).multi_eval(kb.sentences) == 1:
-			if KB(model,alpha).multi_eval(alpha) == 1:
-				return True
-			else:
-				return False
-		else:
-			return True #always implies True if the first object is False
-	else: 
-		P = symbols[0]
-		Rest = symbols[1:]
-		model_P_true = model.copy()
-		model_P_false = model.copy()
-		
-		model_P_false[P] = -1
-		model_P_true[P] = 1
-		return (ttcheckall(kb,alpha,Rest,model_P_true) and ttcheckall(kb,alpha,Rest,model_P_false))
+    if not symbols: #if symbols is empty
+        if KB(model,kb.sentences).multi_eval(kb.sentences,model) == 1:
+            if KB(model,alpha).multi_eval(alpha,model) == 1:
+                return True
+            else:
+                return False
+        else:
+            return True #always implies True if the first object is False
+    else: 
+        P = symbols[0]
+        Rest = symbols[1:]
+        model_P_true = model.copy()
+        model_P_false = model.copy()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        model_P_false[P] = -1
+        model_P_true[P] = 1
+        return (ttcheckall(kb,alpha,Rest,model_P_true) and ttcheckall(kb,alpha,Rest,model_P_false))
--- part1.py
+++ part1.py
@@ -1,5 +1,5 @@
 import automated_reasoning.automated_reasoning as ar	
-
+print('\n \n')
 print('Question 1: Prove Modus Ponens works in this system')
 print('----------------------------------------------------\n')
 print('''Input KB: [[ar.true,'P'],[ar.Implies,'P','Q']] \n
@@ -11,6 +11,21 @@ kb = ar.KB(model,sentences)
 alpha = [ar.true,'Q']
 print('If P is true and P implies Q, computer program says that Q is', ar.ttentails(kb,alpha),'\n')
 
-print('See Python code to verify =).')
+print('See Python code to verify =). \n')
 
+print('Question 2: The Wumpus World')
+print('----------------------------------------------------\n')
+print('''Input KB: [[ar.false,'P11'],[ar.Iff,'B11',[ar.Or,'P12','P21']], \n [ar.Iff,'B21',[ar.Or,'P11',[ar.Or,'P22','P31']]],[ar.false,'B11'],[ar.true,'B21']] \n
+Input Model: {'P11' : 0,'B11' : 0, 'B21' : 0, 'P22' : 0, 'P31' : 0} \n
+Input Sentence: [ar.true,'P12'] \n''')
+sentences = [[ar.false,'P11'],[ar.Iff,'B11',[ar.Or,'P12','P21']],[ar.Iff,'B21',[ar.Or,'P11',[ar.Or,'P22','P31']]],[ar.false,'B11'],[ar.true,'B21']]
+model = {'P11' : 0,'B11' : 0, 'B21' : 0, 'P22' : 0, 'P31' : 0, 'P12' : 0, 'P21' : 0}
+kb = ar.KB(model,sentences)
+alpha = [ar.true,'P12']
+if ar.ttentails(kb,alpha):
+	print('If KB is true, then computer program says we can conclude there is a Pit at (1,2).')
 
+else:
+	print('''If KB is true, then computer program says that we cannot conclude there is a Pit at (1,2). \n This makes sense because if there's no breeze at (1,1) then there cannot be a pit at (!,2).''')
+
+print('See Python code to verify =). \n')
